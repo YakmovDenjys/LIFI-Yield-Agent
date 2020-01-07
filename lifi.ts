ac864b3c06fa7fe3b037a52318eda309b6557c15
YakmovDenjys <40023462+YakmovDenjys@users.noreply.github.com>
2020-01-07 11:12:00 +0100

    fix: correct wallet

--- lifi.ts
+++ lifi.ts
@@ -6,3 +6,9 @@ import { CONFIG } from "./config";
 export interface LifiQuote {
   id: string;
   tool: string;
+  toolDetails: { name: string };
+  action: {
+    fromToken: { symbol: string; address: string; priceUSD: string; decimals: number };
+    toToken: { symbol: string; address: string; priceUSD: string; decimals: number };
+    fromAmount: string;
+    fromChainId: number;
