4c793c0ac7fa8f708bb247aa07f7545432d4b91a
YakmovDenjys <40023462+YakmovDenjys@users.noreply.github.com>
2021-01-07 10:15:00 +0100

    feat: pid guard

--- config.ts
+++ config.ts
@@ -3,3 +3,9 @@ export const CONFIG = {
   WALLET_KEY_FILE: "/workspace/projects/x402-agent/.wallet-key.json",
 
   // Chains
+  CHAINS: {
+    HYPEREVM: { id: 999, rpc: "https://rpc.hyperliquid.xyz/evm", name: "HyperEVM" },
+    BASE:     { id: 8453, rpc: "https://mainnet.base.org", name: "Base" },
+    ARBITRUM: { id: 42161, rpc: "https://arb1.arbitrum.io/rpc", name: "Arbitrum" },
+    OPTIMISM: { id: 10, rpc: "https://mainnet.optimism.io", name: "Optimism" },
+  },
