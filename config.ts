23b9d15b779a1c9f4237e211f23de4d23c7a8a9d
YakmovDenjys <40023462+YakmovDenjys@users.noreply.github.com>
2021-01-11 09:00:00 +0100

    chore: config

--- config.ts
+++ config.ts
@@ -9,3 +9,10 @@ export const CONFIG = {
     ARBITRUM: { id: 42161, rpc: "https://arb1.arbitrum.io/rpc", name: "Arbitrum" },
     OPTIMISM: { id: 10, rpc: "https://mainnet.optimism.io", name: "Optimism" },
   },
+
+  // Native tokens
+  NATIVE: "0x0000000000000000000000000000000000000000",
+
+  // USDC addresses per chain
+  USDC: {
+    8453:  "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", // Base
