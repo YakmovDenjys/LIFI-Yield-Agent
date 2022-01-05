8bb677d8e49bef56205267096a23f80ba534c614
YakmovDenjys <40023462+YakmovDenjys@users.noreply.github.com>
2022-01-05 19:17:00 +0100

    docs: document quote

--- logger.ts
+++ logger.ts
@@ -18,3 +18,8 @@ function ensureDir(file: string) {
 }
 
 export function log(type: AgentEvent["type"], message: string, data?: Record<string, any>) {
+  const event: AgentEvent = { ts: new Date().toISOString(), type, message, data };
+  events.push(event);
+  if (events.length > MAX_EVENTS) events.shift(); // keep last MAX_EVENTS
+
+  const line = `[${event.ts}] [${type.toUpperCase()}] ${message}${data ? " " + JSON.stringify(data) : ""}\n`;
