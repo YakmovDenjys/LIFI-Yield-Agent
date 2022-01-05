fbb53df653e1eeab6bb7fce5e5f0148d0e3afae9
ryotapatel2935 <309026439+ryotapatel2935@users.noreply.github.com>
2022-01-05 18:52:00 +0100

    refactor: tidy yields

--- logger.ts
+++ logger.ts
@@ -2,3 +2,8 @@ import { appendFileSync, existsSync, mkdirSync } from "fs";
 import { dirname } from "path";
 import { CONFIG } from "./config";
 
+export interface AgentEvent {
+  ts: string;
+  type: "scan" | "decision" | "bridge" | "complete" | "error" | "info";
+  message: string;
+  data?: Record<string, any>;
